dac1e2e8bacdb20f2aa61c587df7ae549fb675d8
Viviane Castilho <99738803+Vivicastilho@users.noreply.github.com>
2023-11-01 13:55:05 -0300

    FinishedNavs

---
+++ components/index.jsx
@@ -0,0 +1,6 @@
+import Bottombar from "./shared/Bottombar";
+import Topbar from "./shared/Topbar";
+import RightSideBar from "./shared/RightSideBar";
+import LeftSideBar from "./shared/LeftSideBar";
+
+export { Bottombar, LeftSideBar, RightSideBar, Topbar };
