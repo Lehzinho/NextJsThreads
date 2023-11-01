393b9b67ddd67f8b8a720f3379efe8bf0fdae6b3
Viviane Castilho <99738803+Vivicastilho@users.noreply.github.com>
2023-11-01 14:37:41 -0300

    accountProfile

--- components/index.jsx
+++ components/index.jsx
@@ -2,5 +2,6 @@ import Bottombar from "./shared/Bottombar";
 import Topbar from "./shared/Topbar";
 import RightSideBar from "./shared/RightSideBar";
 import LeftSideBar from "./shared/LeftSideBar";
+import AccountProfile from "./forms/AccountProfile";
 
-export { Bottombar, LeftSideBar, RightSideBar, Topbar };
+export { Bottombar, LeftSideBar, RightSideBar, Topbar, AccountProfile };
